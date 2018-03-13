973769e1757fae5a976afff0d488750884384a9d
smokbel <sajeda-mokbel@hotmail.com>
2018-03-13 14:19:30 -0400

    added topojson stuff instead

--- js/main.js
+++ js/main.js
@@ -1,23 +1,35 @@
-var ontsvg = 'CensusSubDiv.svg';
+// var ontsvg = 'CensusSubDiv.svg';
+//
+// kartograph.map('#map', 900, 900)
+//           .loadMap(ontsvg, mapLoaded);
+//
+// function mapLoaded(map) {
+//     map.addLayer('layer0', {
+//         styles: {
+//             stroke: '#aaa',
+//             fill: '#f6f4f2'
+//         },
+//         mouseenter: function(d, path) {
+//             path.attr('fill', '#c04');
+//         },
+//         mouseleave: function(d, path) {
+//             path.animate({ fill: '#f6f4f2' }, 500);
+//         }
+//     });
+// }
 
-kartograph.map('#map', 900, 900)
-          .loadMap(ontsvg, mapLoaded);
+// MAP WITH TOPOJSON
 
-function mapLoaded(map) {
-    map.addLayer('layer0', {
-        styles: {
-            stroke: '#aaa',
-            fill: '#f6f4f2'
-        },
-        mouseenter: function(d, path) {
-            path.attr('fill', '#c04');
-        },
-        mouseleave: function(d, path) {
-            path.animate({ fill: '#f6f4f2' }, 500);
-        }
-    });
+var width = 960, height = 700;
 
+var svg = d3.select('#map').append('svg')
+  .attr('width', width)
+  .attr('height', height)
 
+d3.json('CensusSubDiv.json', function(error, CensusSubDiv) {
+  if (error) return console.error(error);
 
-
-}
+  svg.append('path')
+  .datum(topojson.feature(CensusSubDiv, CensusSubDiv.objects.CensusSubDivision))
+  .attr('d', d3.geo.path().projection(d3.geo.mercator()));
+});
